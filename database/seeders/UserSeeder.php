1e7f3ded7e638fca6fa038cfc17594df260510ad
Fellycia Caroline <165134673+FellyciaCaroline@users.noreply.github.com>
2024-12-24 18:37:54 +0700

    seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +15,10 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        User::factory()->create([
+            "name" => "felly",
+            "email" => "felly@gmail.com",
+            'password' => Hash::make('12345678'),
+        ]);
     }
 }
